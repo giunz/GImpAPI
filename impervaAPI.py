78b7ec59b288fe28dde39362f319740452edf742
GiUnZ <ggiouzelis@gmail.com>
2018-04-04 12:03:29 +0300

    indents fix

--- impervaAPI.py
+++ impervaAPI.py
@@ -13,79 +13,91 @@ import requests
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
 headers = {'content-type': 'application/json'}
 config = configparser.ConfigParser()
-try:
-  config.read('settings.ini')
-  host = config['DEFAULT']['host']
-  user = config['DEFAULT']['user']
-  password= (config['DEFAULT']['password'])
-except KeyError:
-  logging.info("No configurations file found.")
+
+config.read('settings.ini')
+host = config['DEFAULT']['host']
+user = config['DEFAULT']['user']
+password= (config['DEFAULT']['password'])
 
 logging.debug("Using Hostname "+host)
 logging.debug("Using Username "+user)
 logging.debug("Using Password "+password)
 
 def login():
- logging.info('Logging in.')
- r = requests.post(host+'/SecureSphere/api/v1/auth/session', headers=headers ,auth=(user, password),verify=False)
- c = r.cookies['JSESSIONID']
- if (r.status_code == 200):
-  logging.info('Successfully logged in.')
- else:
-  logging.error('An error occured. Status code is '+r.status_code)
- return c
+    logging.info('Logging in.')
+    r = requests.post(host+'/SecureSphere/api/v1/auth/session', headers=headers ,auth=(user, password),verify=False)
+    c = r.cookies['JSESSIONID']
+    if (r.status_code == 200):
+        logging.info('Successfully logged in.')
+    else:
+        logging.error('An error occured. Status code is '+r.status_code)
+        return c
 
 def logout(biscuit):
- logging.info('Logging out')
- r = requests.delete(host+'/SecureSphere/api/v1/auth/session', headers=headers, cookies=biscuit,verify=False)
- if (r.status_code == 200):
-  logging.info('Logged out successfully.')
- else:
-  logging.error('An error occured. Status code is '+r.status_code)
+    logging.info('Logging out')
+    r = requests.delete(host+'/SecureSphere/api/v1/auth/session', headers=headers, cookies=biscuit,verify=False)
+    if (r.status_code == 200):
+      logging.info('Logged out successfully.')
+    else:
+      logging.error('An error occured. Status code is '+r.status_code)
 
 def getAllSites(biscuit):
- logging.info('Getting Sites.')
- r = requests.get(host+'/SecureSphere/api/v1/conf/sites', headers=headers, cookies=biscuit,verify=False)
- if (r.status_code == 200):
-  sites = r.json()
-  logging.info('Retrieved the sites successfully.')
-  logging.debug(str(sites))
-  return sites
- else:
-  logging.error('An error occured. Status code is '+r.status_code)
+    logging.info('Getting Sites.')
+    r = requests.get(host+'/SecureSphere/api/v1/conf/sites', headers=headers, cookies=biscuit,verify=False)
+    if (r.status_code == 200):
+      sites = r.json()
+      logging.info('Retrieved the sites successfully.')
+      logging.debug(str(sites))
+      return sites
+    else:
+      logging.error('An error occured. Status code is '+r.status_code)
 
 def getAllServerGroups(biscuit,site):
- logging.info('Getting Server Groups.')
- r = requests.get(host+'/SecureSphere/api/v1/conf/serverGroups/'+site, headers=headers, cookies=biscuit,verify=False)
- if (r.status_code == 200):
-  server_groups = r.json()
-  logging.info('Retrieved the Server Groups successfully.')
-  logging.debug(str(server_groups))
-  return server_groups
- else:
-  logging.error('An error occured. Status code is '+r.status_code)
+    logging.info('Getting Server Groups.')
+    r = requests.get(host+'/SecureSphere/api/v1/conf/serverGroups/'+site, headers=headers, cookies=biscuit,verify=False)
+    if (r.status_code == 200):
+      server_groups = r.json()
+      logging.info('Retrieved the Server Groups successfully.')
+      logging.debug(str(server_groups))
+      return server_groups
+    else:
+      logging.error('An error occured. Status code is '+r.status_code)
 
 def getAllWebServices(biscuit,site,server_group):
- logging.info('Getting Web Services.')
- r = requests.get(host+'/SecureSphere/api/v1/conf/webServices/'+site+'/'+server_group, headers=headers, cookies=biscuit,verify=False)
- if (r.status_code == 200):
-  ws = r.json()
-  logging.info('Retrieved the Web Services successfully.')
-  logging.debug(str(ws))
-  return ws
- else:
-  logging.error('An error occured. Status code is '+r.status_code)
+    logging.info('Getting Web Services.')
+    r = requests.get(host+'/SecureSphere/api/v1/conf/webServices/'+site+'/'+server_group, headers=headers, cookies=biscuit,verify=False)
+    if (r.status_code == 200):
+      ws = r.json()
+      logging.info('Retrieved the Web Services successfully.')
+      logging.debug(str(ws))
+      return ws
+    else:
+      logging.error('An error occured. Status code is '+r.status_code)
 
 def getAllWebApplications(biscuit,site,server_group,web_service):
- logging.info('Getting Web Applications.')
- r = requests.get(host+'/SecureSphere/api/v1/conf/webApplications/'+site+'/'+server_group+'/'+web_service, headers=headers, cookies=biscuit,verify=False)
- if (r.status_code == 200):
-  wapps = r.json()
-  logging.info('Retrieved the Web Applications successfully.')
-  logging.debug(str(wapps))
-  return wapps
- else:
-  logging.error('An error occured. Status code is '+r.status_code)
+    logging.info('Getting Web Applications.')
+    r = requests.get(host+'/SecureSphere/api/v1/conf/webApplications/'+site+'/'+server_group+'/'+web_service, headers=headers, cookies=biscuit,verify=False)
+    if (r.status_code == 200):
+        wapps = r.json()
+        logging.info('Retrieved the Web Applications successfully.')
+        logging.debug(str(wapps))
+        return wapps
+    else:
+        logging.error('An error occured. Status code is '+r.status_code)
+
+def getProfile(biscuit,site,server_group,web_service,web_application):
+    logging.info('Getting Profile.')
+    r = requests.get(host+'/SecureSphere/api/v1/conf/webProfile/'+site+'/'+server_group+'/'+web_service+'/'+web_application, headers=headers, cookies=biscuit,verify=False)
+    if (r.status_code == 200):
+      app_profile = r.json()
+      logging.info('Retrieved the Web Profile successfully.')
+      logging.debug(str(app_profile))
+      return app_profile
+    else:
+      logging.error('An error occured. Status code is '+r.status_code)
+
+def writeProfileToFile(profile):
+
 
 cookies=login()
 logging.debug('JSESSIONID is '+cookies)
@@ -98,7 +110,15 @@ logging.debug('First Server Group\'s name for site '+site+' is.... '+server_grou
 sg=server_groups['server-groups'][0]
 ws=getAllWebServices(cookie,site,sg)
 logging.debug('First service\'s name for site '+site+' and Server Group '+sg+' is '+ws['web-services'][0])
-web_service=ws['web-services'][1]
+web_service=ws['web-services'][0]
 web_apps=getAllWebApplications(cookie,site,sg,web_service)
 logging.debug('The web application is '+web_apps['webApplications'][0])
+web_app = web_apps['webApplications'][0]
+profile = getProfile(cookie,site,sg,web_service,web_app)
+fname="profile"+web_app+".json"
+logging.debug('Writing profile to '+fname+' file')
+f=open(fname,"w+")
+json_res = json.dumps(profile, indent=4, sort_keys=False)
+f.write(json_res)
+f.close
 logout(cookie)
